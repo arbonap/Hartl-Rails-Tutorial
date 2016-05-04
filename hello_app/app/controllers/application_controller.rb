1f61b60ccc83cf5be2b2642f17a9e8f1b117282b
Patricia Arbona <arbonap@gmail.com>
2016-05-04 16:20:55 -0700

    updated index controller

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render text: "Hello, World!"
+    render text: "¡Hola, mundo!"
+  end
+  def goodbyefelicia
+    render text: "Hasta luego, Felicia."
   end
 end
